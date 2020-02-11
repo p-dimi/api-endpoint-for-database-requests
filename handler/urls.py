23150b99d3d00e40ef598d6f4af9657380204e24
p-dimi <41579855+p-dimi@users.noreply.github.com>
2020-02-11 14:55:10 +0100

    Add files via upload
    removed trailing slash

--- handler/urls.py
+++ handler/urls.py
@@ -4,5 +4,5 @@ from .views import ViewClicksInfo
 
 urlpatterns = [
     path('clicks_info/<str:api_flags>', ViewClicksInfo.as_view()),
-    path('clicks_info/', ViewClicksInfo.as_view()),
+    path('clicks_info', ViewClicksInfo.as_view()),
 ]
